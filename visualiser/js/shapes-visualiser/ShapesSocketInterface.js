4c2ce87d6291b280a825db5dd188854f35a057a9
Owen Hindley <owenhindley@hotmail.com>
2014-02-27 13:39:26 +0000

    add basic visualiser

---
+++ visualiser/js/shapes-visualiser/ShapesSocketInterface.js
@@ -0,0 +1,53 @@
+(function() {
+
+	var EventDispatcher = retropi.getNamespace("events").EventDispatcher;
+	
+	retropi.createClass("shapes-visualiser", "ShapesSocketInterface", EventDispatcher, function(aClassObject, aClassPrototype, aClassName){
+
+		var p = aClassPrototype;
+		var s = EventDispatcher.prototype;
+
+		aClassObject.EVENT_BEAT = "shapesSocketInterfaceBeat";
+
+		p.init = function() {
+
+			s.init.call(this);
+
+			this._socket = null;
+
+			this._socketConnectBound = this._onSocketConnect.bind(this);
+
+			this._socketBeatBound = this._onSocketBeat.bind(this);
+
+		}
+
+		p.setup = function(aClientIp){
+
+			this._socket = io.connect(aClientIp);
+
+			this._socket.on('connect', this._onSocketConnectBound);
+
+
+		}
+
+
+		p._onSocketConnect = function(aSocket){
+
+			console.log("ShapesSocketInterface :: on Connect!");
+
+
+			this._socket.on('beat', this._socketBeatBound);
+
+		};
+
+
+		p._onSocketBeat = function(aSocket){
+
+			this.dispatchCustomEvent(aClassObject.EVENT_BEAT, null);
+
+		};
+
+	});
+ 
+
+})();
